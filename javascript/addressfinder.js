659f0c140d1a1fcb8e71a8b57eda07bcc5dc34f2
Will Rossiter <will@fullscreen.io>
2016-05-08 20:16:18 +1200

    Fire event

--- javascript/addressfinder.js
+++ javascript/addressfinder.js
@@ -1,68 +1,69 @@
-;(function($) {
-	$(document).ready(function () {
-		$(".address_finder").each(function (i, elem) {
-			var widget, 
-				key = $(elem).data('api-key'),
-				address = $(elem).find('.address_finder_address'),
-				input = $(elem).find("input").first(),
-				manual = $(elem).find('.manual_address'),
-				toggle = $(elem).find('.toggle_manual_address');
+;(function ($) {
+  $(document).ready(function () {
+    $('.address_finder').each(function (i, elem) {
+      var widget,
+        key = $(elem).data('api-key'),
+        address = $(elem).find('.address_finder_address'),
+        input = $(elem).find('input').first(),
+        manual = $(elem).find('.manual_address'),
+        toggle = $(elem).find('.toggle_manual_address')
 
-			var useManual = manual.find("input[name*=ManualAddress]"),
-				field = address.find('input').get(0);
+      var useManual = manual.find('input[name*=ManualAddress]'),
+        field = address.find('input').get(0)
 
-			/* update ui with javascript */
-			toggle.show();
-			address.show();
+      /* update ui with javascript */
+      toggle.show()
+      address.show()
 
-			if(!useManual.val()) {
-				manual.hide();
-			}
+      if (!useManual.val()) {
+        manual.hide()
+      }
 
-			/* create widget */
-			widget = new AddressFinder.Widget(field, key, {
-				show_locations: false
-			});
+      /* create widget */
+      widget = new AddressFinder.Widget(field, key, {
+        show_locations: false
+      })
 
-			/* updates manual fields and hidden metadata */
-			widget.on("result:select", function (value, item) {
-				/* populate postal line fields */
-				for(var i = 1; i <= 6; i++) {
-					manual.find("input[name*=PostalLine" + i +"]").val(item["postal_line_"+ i] || '');
-				}
+      /* updates manual fields and hidden metadata */
+      widget.on('result:select', function (value, item) {
+        /* populate postal line fields */
+        for (var i = 1; i <= 6; i++) {
+          manual.find('input[name*=PostalLine' + i + ']').val(item['postal_line_' + i] || '')
+        }
 
-				manual.find("input[name*=Suburb]").val(item.suburb || '');
-				manual.find("input[name*=City]").val(item.city || '');
-				manual.find("input[name*=Postcode]").val(item.postcode || '');
-				manual.find("input[name*=Longitude]").val(item.x || '');
-				manual.find("input[name*=Latitude]").val(item.y || '');
-			});
+        manual.find('input[name*=Suburb]').val(item.suburb || '')
+        manual.find('input[name*=City]').val(item.city || '')
+        manual.find('input[name*=Postcode]').val(item.postcode || '')
+        manual.find('input[name*=Longitude]').val(item.x || '')
+        manual.find('input[name*=Latitude]').val(item.y || '')
 
-			/* click handler to toggle manual div */
-			toggle.on('click', function(e) {
-				e.preventDefault();
+        $('body').trigger(jQuery.Event('addressselected'))
+      })
 
-				manual.toggle('slow');
+      /* click handler to toggle manual div */
+      toggle.on('click', function (e) {
+        e.preventDefault()
 
-				// if the manual address is visible then add a hidden flag so
-				if(manual.is(":visible")) {
-					useManual.val('1');
-				}
-				else {
-					useManual.val('0');
-				}
-			});
+        manual.toggle('slow')
 
-			/* on manually changing of the fields then we have to clear x/y */
-			manual.on('keydown', 'input', function(e) {
-				manual.find("input[name*=Longitude]").val('');
-				manual.find("input[name*=Latitude]").val('');
-			});
+        // if the manual address is visible then add a hidden flag so
+        if (manual.is(':visible')) {
+          useManual.val('1')
+        } else {
+          useManual.val('0')
+        }
+      })
 
-			/* focusing back on the address dropdown should hide the manual */
-			input.on('focus', function(e) {
-				manual.slideUp();
-			});
-		});
-	});
-})(jQuery);
+      /* on manually changing of the fields then we have to clear x/y */
+      manual.on('keydown', 'input', function (e) {
+        manual.find('input[name*=Longitude]').val('')
+        manual.find('input[name*=Latitude]').val('')
+      })
+
+      /* focusing back on the address dropdown should hide the manual */
+      input.on('focus', function (e) {
+        manual.slideUp()
+      })
+    })
+  })
+})(jQuery)
